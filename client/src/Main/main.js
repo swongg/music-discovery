35fca326946843f39f6a631a578eb1ec47f9644f
michelle714 <michuang@alumni.ubc.ca>
2021-08-05 00:28:16 -0700

    Added two buttons to filter liked/top lists
    Server side: Added two endpoints, savedTrack and playlist.
    Client side: Added two buttons that displays list of liked songs and top songs.
    
    also fixed issues:
    1. button floating around
    2. async loading username (not optimal)

--- client/src/Main/main.js
+++ client/src/Main/main.js
@@ -1,10 +1,15 @@
-import React, { useState, useLayoutEffect } from "react";
+import React, { useState, useLayoutEffect, useEffect } from "react";
 import Title from "../UI/title";
 import "./main.css";
+import { Paper, Tabs, Tab } from "@material-ui/core";
 
 const Main = () => {
   const [username, setUsername] = useState();
-  const [topSong, setTopSong] = useState();
+  const [topSongs, setTopSongs] = useState();
+  let [savedTracks, setSavedTracks] = useState();
+
+  const [option, setOption] = useState(0);
+  const [displayList, setDisplayList] = useState([]);
 
   useLayoutEffect(() => {
     fetch("http://localhost:8888/user")
@@ -19,18 +24,52 @@ const Main = () => {
     fetch("http://localhost:8888/toptracks")
       .then((response) => response.json())
       .then((songs) => {
-        let song = songs.body.items[0].name;
-        console.log(song);
-        setTopSong(song);
+        //let song = songs.body.items[0].name;
+        let songs_ = songs.body.items;
+        setTopSongs(songs_);
       });
   });
+
+  useLayoutEffect(() => {
+    fetch("http://localhost:8888/savedTracks")
+      .then((response) => response.json())
+      .then((songs) => {
+        let songs_ = songs.body.items.map((t) => t.track);
+        setSavedTracks(songs_);
+      });
+  });
+
+  const handleChange = (event, newValue) => {
+    setOption(newValue);
+  };
+
+  useEffect(() => {
+    if (option === 0 && topSongs) {
+      setDisplayList(topSongs);
+    }
+    if (option === 1 && savedTracks) {
+      setDisplayList(savedTracks);
+    }
+  }, [option]);
 
   return (
     <div className="default-background__main">
       <div className="center-outer__main">
         <div className="center-inner__main">
-          <Title content="Welcome" user={username} type="main" />
-          <Title content={"Your top song: " + topSong} type="sub"></Title>
+          {username && <Title content="Welcome" user={username} type="main" />}
+          <Paper square className="Tabs__main">
+            <Tabs value={option} onChange={handleChange} centered>
+              <Tab label="Most Played Songs" />
+              <Tab label="Liked Songs" />
+            </Tabs>
+          </Paper>
+          {displayList.length > 0 && (
+            <div>
+              {displayList.map((item) => (
+                <div key={item.id}>{item.name}</div>
+              ))}
+            </div>
+          )}
         </div>
       </div>
     </div>
