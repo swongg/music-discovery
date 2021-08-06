58c522e6f69bbfaa23fdb818ef6abb03cc67ed02
michelle714 <michuang@alumni.ubc.ca>
2021-08-05 18:25:53 -0700

    basic prototype for generatedList
    just reading from saved list for now

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,20 @@
 import React from "react";
 import Landing from "./Landing/landing";
 import Main from "./Main/main";
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import GeneratedList from "./GeneratedList/generatedList";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-
 
 function App() {
   return (
-    <Router> 
-    <div className="App">
-      <Switch>
-        <Route path='/' exact component={Landing} />
-        <Route path='/main' component={Main} />
-      </Switch>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={Landing} />
+          <Route path="/main" component={Main} />
+          <Route path="/generatedList" component={GeneratedList} />
+        </Switch>
+      </div>
     </Router>
   );
 }
